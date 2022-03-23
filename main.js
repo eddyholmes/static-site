5c08d157b3bdc63dc7797d5bdf4fcfdc08b41d1b
eddyholmes <eddy@webdevgroup.com>
2022-03-23 16:33:19 +0500

    reposition script

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 (function () {
   console.log('works');
   setTimeout(() => {
-    // throw new Error("Testing sentry filter from statis site");
+    throw new Error("Testing sentry filter from statis site");
   }, 3000);
   throw new Error("Testing immediate statis site error");
 })()
