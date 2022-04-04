cccf6a10eed4f5df77229f704e252c550189c8fa
eddyholmes <eddy@webdevgroup.com>
2022-04-04 12:33:35 +0500

    update form

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
 (function () {
   console.log('works');
-  setTimeout(() => {
-    throw new Error("Testing sentry filter from statis site");
-  }, 3000);
-  throw new Error("Testing immediate statis site error");
+  // throw new Error("Testing immediate statis site error");
 })();
